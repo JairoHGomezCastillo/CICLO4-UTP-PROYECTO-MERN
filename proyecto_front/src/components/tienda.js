cd221dcdc8ed92b512de76937447e002c5a4d87d
jahugomez <jahugomez@gmail.com>
2022-11-03 23:25:07 -0500

    adición primer modelo tienda

--- proyecto_front/src/components/tienda.js
+++ proyecto_front/src/components/tienda.js
@@ -1,8 +1,34 @@
-const Tienda = () => { 
-    return (
-        <div>
-            <h1>Aqui va el ecommerce</h1>
+import data from "../data/data";
+import "./tienda.css";
+
+const Tienda = () => {
+  return (
+    <div>
+      <header>
+        <a href="/">Home</a>
+      </header>
+      <main>
+        <h1>Listado de Productos</h1>
+        <div className="productos">
+          {data.productos.map((producto) => (
+            <div className="producto" key={producto.detalle}>
+              <a href={`/producto/${producto.detalle}`}>
+                <img src={producto.imagen} alt={producto.nombre} />
+              </a>
+              <div className="prod-info">
+                <a href={`/producto/${producto.detalle}`}>
+                  <p>{producto.nombre}</p>
+                </a>
+                <p>
+                  <strong>${producto.precio}</strong>
+                </p>
+                <button>Agregar al carrito</button>
+              </div>
+            </div>
+          ))}
         </div>
-    )
-}
+      </main>
+    </div>
+  );
+};
 export default Tienda;
---
+++ proyecto_front/src/data/data.js
@@ -0,0 +1,54 @@
+const data = {
+  productos: [
+    {
+      nombre: "Varitas",
+      detalle: "varitas-perros",
+      categoria: "Snacks",
+      imagen: "/images/p1.jpg", //679px x 829px
+      precio: 15000,
+      stock: 10,
+      marca: "Mershop",
+      clasificación: 4.5,
+      referencia: "10 Und",
+      descripcion: "Tipo Cabano Mordida Dura",
+    },
+    {
+      nombre: "Huesos",
+      detalle: "huesos-perros",
+      categoria: "Snacks",
+      imagen: "/images/p2.jpg",
+      precio: 20000,
+      stock: 10,
+      marca: "Mershop",
+      clasificación: 4,
+      referencia: "2 Und",
+      descripcion: "Tipo hueso para Perro Mordida dura",
+    },
+    {
+      nombre: "Galletas Perros",
+      detalle: "galletas-perros",
+      categoria: "Snacks",
+      imagen: "/images/p3.jpg",
+      precio: 25000,
+      stock: 10,
+      marca: "Mungos",
+      clasificación: 4.5,
+      referencia: "200 grs",
+      descripcion: "Galletas Rollitos Caseras",
+    },
+    {
+      nombre: "Salchichas Perros",
+      detalle: "salchichas-perros",
+      categoria: "Snacks",
+      imagen: "/images/p4.jpg",
+      precio: 25000,
+      stock: 10,
+      marca: "Mungos",
+      clasificación: 4.5,
+      referencia: "200 grs",
+      descripcion: "Salchichas",
+    },
+  ],
+};
+
+export default data;
