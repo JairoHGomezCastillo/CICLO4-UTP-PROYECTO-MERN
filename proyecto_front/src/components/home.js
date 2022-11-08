a7e0f7addf6185d296943af49c7bed8eaa8f6c0a
jahugomez <jahugomez@gmail.com>
2022-11-07 19:33:20 -0500

    home

--- proyecto_front/src/components/home.js
+++ proyecto_front/src/components/home.js
@@ -1,38 +1,25 @@
-import React from 'react';
-import {  } from 'react-bootstrap';
-import Carrusel from './carousel';
-import Footer from './footer';
+import React from "react";
+import {} from "react-bootstrap";
+import Carrusel from "./carousel";
+import Footer from "./footer";
 
 const Home = () => {
-  return (    
+  return (
     <div>
-     
-      <h1>Home Reino Rolitos</h1>         
-               
-             
-          <div>
-          <div >
-            <div >
-
-           
-            <div className='col 18'>
-            <Carrusel /> 
-            </div>
-            <p className='text1' > Rolitos es conocido como uno de los   reinos más queridos, aclamados y respetables que han existido.
-              Algunos dicen que son sus imponentes palacios y castillos los que le conceden este reconocimiento.
-              </p>
-            <div>
-            <Footer /> 
-            </div>
-          
-          </div>
-
-
-        </div>         
-               
-        </div>    
-      </div>    
-    
+      <h1>Home Reino Rolitos</h1>
+      <div className="col 18">
+        <Carrusel />
+      </div>
+      <p className="text1">
+        {" "}
+        Rolitos es conocido como uno de los reinos más queridos, aclamados y
+        respetables que han existido. Algunos dicen que son sus imponentes
+        palacios y castillos los que le conceden este reconocimiento.
+      </p>
+      <div>
+        <Footer />
+      </div>
+    </div>
   );
 };
 
