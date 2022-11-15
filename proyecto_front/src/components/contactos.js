3bdfb97cdb7019e4e736c91c1b52084d2b228971
dianacavar <dianacavar@gmail.com>
2022-11-15 10:55:48 -0500

    Elaboracion del contactanos

--- proyecto_front/src/components/contactos.js
+++ proyecto_front/src/components/contactos.js
@@ -1,8 +1,33 @@
+import React from "react";
+import { Form, Button, Col } from "react-bootstrap";
+
 const Contact = () => {
   return (
-    <div>
-      <h1>Contact Reino Rolitos</h1>
-    </div>
+    <Form className="contactenos">
+      <Col sm={4}>
+        <div>
+          <h3>CONTACTANOS</h3>
+        </div>
+        <Form.Group class="mb-3">
+          <Form.Label>Nombre</Form.Label>
+          <Form.Control type="text" placeholder="Escribe tu nombre" />
+        </Form.Group>
+
+        <Form.Group class="mb-3">
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="email" placeholder="Escribe tu Email" />
+        </Form.Group>
+
+        <Form.Group class="mb-3"> 
+          <Form.Label>Comentarios</Form.Label>
+          <Form.Control type="text" placeholder="¿En qué podemos ayudarte?" />
+        </Form.Group>
+
+        <Button  variant="primary" type="submit">
+          Enviar
+        </Button>
+      </Col>
+    </Form>
   );
 };
 export default Contact;
