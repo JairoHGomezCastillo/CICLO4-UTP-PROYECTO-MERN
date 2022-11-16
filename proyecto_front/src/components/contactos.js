ce13ae73065633eeb148551f5db4648290bc5664
jahugomez <jahugomez@gmail.com>
2022-11-15 23:01:06 -0500

    manejo de estados by Reducer

--- proyecto_front/src/components/contactos.js
+++ proyecto_front/src/components/contactos.js
@@ -1,39 +1,42 @@
 import React from "react";
 import { Form, Button, Col, Figure } from "react-bootstrap";
 import Footer from "./elements/footer";
+import "./contactos.css";
 
 const Contactos = () => {
   return (
     <div>
       <Form className="contactenos">
-        
         <Figure className="col 1">
-          <Figure.Image width={400}  src="/rol7.jpg"/>
+          <Figure.Image width={400} src="/images/rol7.jpg" />
         </Figure>
 
-        <Col sm={5} >
+        <Col sm={5}>
           <div>
             <h3>CONTACTANOS</h3>
           </div>
           <div class="col 2">
-          <Form.Group class="mb-3">
-            <Form.Label>Nombre</Form.Label>
-            <Form.Control type="text" placeholder="Escribe tu nombre" />
-          </Form.Group>
+            <Form.Group class="mb-3">
+              <Form.Label>Nombre</Form.Label>
+              <Form.Control type="text" placeholder="Escribe tu nombre" />
+            </Form.Group>
 
-          <Form.Group class="mb-3">
-            <Form.Label>Email</Form.Label>
-            <Form.Control type="email" placeholder="Escribe tu Email" />
-          </Form.Group>
+            <Form.Group class="mb-3">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" placeholder="Escribe tu Email" />
+            </Form.Group>
 
-          <Form.Group class="mb-3">
-            <Form.Label>Comentarios</Form.Label>
-            <Form.Control type="text" placeholder="¿En qué podemos ayudarte?" />
-          </Form.Group>
+            <Form.Group class="mb-3">
+              <Form.Label>Comentarios</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="¿En qué podemos ayudarte?"
+              />
+            </Form.Group>
 
-          <Button variant="primary" type="submit">
-            Enviar
-          </Button>
+            <Button variant="primary" type="submit">
+              Enviar
+            </Button>
           </div>
         </Col>
       </Form>
--- proyecto_front/src/components/tienda.js
+++ proyecto_front/src/components/tienda.js
@@ -1,16 +1,45 @@
-import { useEffect, useState } from "react";
+import { useEffect, useReducer } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
+import logger from "use-reducer-logger";
 // import data from "../data/data";
 import Footer from "./elements/footer";
 import "./tienda.css";
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_REQUEST":
+      return { ...state, loading: true };
+    case "FETCH_SUCCESS":
+      return { ...state, productos: action.payload, loading: false };
+    case "FETCH_FAIL":
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
 const Tienda = () => {
-  const [productos, setProductos] = useState([]);
+  const [{ loading, error, productos }, dispatch] = useReducer(
+    logger(reducer),
+    {
+      productos: [],
+      loading: true,
+      error: "",
+    }
+  );
+  //const [productos, setProductos] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios.get("/productos");
-      setProductos(result.data);
+      dispatch({ type: "FETCH_REQUEST" });
+      try {
+        const result = await axios.get("/productos");
+        dispatch({ type: "FETCH_SUCCESS", payload: result.data });
+      } catch (error) {
+        dispatch({ type: "FETCH_FAIL", payload: error.message });
+      }
+
+      //setProductos(result.data);
     };
     fetchData();
   }, []);
@@ -19,22 +48,28 @@ const Tienda = () => {
       <main>
         <h1>Listado de Productos</h1>
         <div className="productos">
-          {productos.map((producto) => (
-            <div className="producto" key={producto.detalle}>
-              <Link to={`/producto/${producto.detalle}`}>
-                <img src={producto.imagen} alt={producto.nombre} />
-              </Link>
-              <div className="prod-info">
+          {loading ? (
+            <div>Loading...</div>
+          ) : error ? (
+            <div>{error}</div>
+          ) : (
+            productos.map((producto) => (
+              <div className="producto" key={producto.detalle}>
                 <Link to={`/producto/${producto.detalle}`}>
-                  <p>{producto.nombre}</p>
+                  <img src={producto.imagen} alt={producto.nombre} />
                 </Link>
-                <p>
-                  <strong>${producto.precio}</strong>
-                </p>
-                <button>Agregar al carrito</button>
+                <div className="prod-info">
+                  <Link to={`/producto/${producto.detalle}`}>
+                    <p>{producto.nombre}</p>
+                  </Link>
+                  <p>
+                    <strong>${producto.precio}</strong>
+                  </p>
+                  <button>Agregar al carrito</button>
+                </div>
               </div>
-            </div>
-          ))}
+            ))
+          )}
         </div>
         <div>
           <Footer />
